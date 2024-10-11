7799fc3f7b0ef38de108482d865caf599e0ed939
Alif Hossain <alifhossain56782@gmail.com>
2024-10-11 11:57:53 +0600

    Font Setup DOne

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { Button } from "flowbite-react";
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+      
+      <h1 className='text-4xl font-title font-bold tracking-wider'>Get 50% Discount</h1>
+      <p className="text-2xl font-para">Lorem ipsum dolor sit amet consectetur adipisicing elit. At facilis natus aliquid harum animi accusamus doloremque vel vitae modi nisi.</p>
+      <Button>Click me</Button>
+    </>
+  )
+}
+
+export default App
