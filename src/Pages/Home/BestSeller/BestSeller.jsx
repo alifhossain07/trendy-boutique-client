282c9d519d9d820552df7454ded1820b57bcf72d
Alif Hossain <alifhossain56782@gmail.com>
2024-10-12 15:05:55 +0600

    Best Seller Section Done

---
+++ src/Pages/Home/BestSeller/BestSeller.jsx
@@ -0,0 +1,90 @@
+import React from "react";
+import { Button, Card } from "flowbite-react";
+
+const BestSeller = () => {
+  const products = [
+    {
+      name: "Men's Casual Shirt",
+      image: "https://i.ibb.co/KNzNY7n/Men-Shirt2.jpg",
+      price: "$45.99",
+    },
+    {
+      name: "Men's Ripped Jeans",
+      image: "https://i.ibb.co/VvDsJvg/men-Jeans2.png",
+      price: "$55.00",
+    },
+    {
+      name: "Women's Evening Gown",
+      image: "https://i.ibb.co/d58nS8G/woman-Evening1.png",
+      price: "$120.00",
+    },
+    {
+      name: "Women's Skinny Jeans",
+      image: "https://i.ibb.co/8j01s4x/woman-Skinny.png",
+      price: "$55.00",
+    },
+  ];
+
+  return (
+    <div className="w-11/12 mx-auto py-20">
+      <h1 className="text-2xl lg:text-3xl font-title uppercase tracking-wider font-bold mb-16">
+        Our Best <span className="text-blue-700">Sellers</span>
+      </h1>
+
+      {/* Best Seller Showing Cards */}
+      <div className="flex gap-8 w-11/12 mx-auto flex-col md:flex-row justify-between">
+        {/* Left Column */}
+        <div className=" w-full md:w-1/2 p-2">
+          <img
+            src="https://i.ibb.co.com/4grnrTj/fashonable-Men.png"
+            alt="Fashion Men"
+            className="w-full shadow-xl h-1/2 object-cover  mb-4"
+          />
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+            {products.slice(0, 2).map((product, index) => (
+              <Card key={index} className="!p-0 overflow-hidden mt-7 ">
+                {/* Background Image */}
+                <div
+                  className="w-full h-48 bg-cover bg-center"
+                  style={{ backgroundImage: `url(${product.image})` }}
+                />
+                <div className="p-4">
+                  <h3 className="text-xl font-title font-bold">{product.name}</h3>
+                  <p className="font-para text-lg text-blue-700">{product.price}</p>
+                </div>
+                <Button className="bg-blue-500 hover:!bg-blue-400">Buy now</Button>
+              </Card>
+            ))}
+          </div>
+        </div>
+
+        {/* Right Column */}
+        <div className=" w-full md:w-1/2 p-2">
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+            {products.slice(2).map((product, index) => (
+              <Card key={index} className="!p-0 overflow-hidden ">
+                {/* Background Image */}
+                <div
+                  className="w-full h-48 bg-cover bg-center"
+                  style={{ backgroundImage: `url(${product.image})` }}
+                />
+                <div className="p-4">
+                  <h3 className="text-xl font-title font-bold">{product.name}</h3>
+                  <p className="font-para text-lg text-blue-700">{product.price}</p>
+                </div>
+                <Button className="bg-blue-500 hover:!bg-blue-400">Buy now</Button>
+              </Card>
+            ))}
+          </div>
+          <img
+            src="https://i.ibb.co.com/1mmH6VF/fashion-Women.jpg"
+            alt="Fashion Men"
+            className="w-full h-1/2 object-cover shadow-xl  mt-4"
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default BestSeller;
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 
 import Banner from './Banner/Banner';
 import ShopCategory from './ShopCategory/ShopCategory';
+import BestSeller from './BestSeller/BestSeller';
 
 const Home = () => {
     return (
         <div className=''>
             <Banner></Banner>
             <ShopCategory></ShopCategory>
+            <BestSeller></BestSeller>
         </div>
     );
 };
